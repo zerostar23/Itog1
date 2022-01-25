1a966686baa1d2d51a54a44e0ca2870b1d82f9a7
George P <killerkip07@mail.ru>
2022-01-25 15:19:31 +0300

    Дополнил файлы

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿// 
+﻿
+string[] Izm(string[] ArrayS)
+    {
+    string[] ArrayVtor = new string[0]; 
+    int j = 0;
+
+    for (int i = 0; i < ArrayS.Length; i++)
+    {
+        if (ArrayS[i].Length <= 3) 
+        {
+            Array.Resize(ref ArrayVtor, ArrayVtor.Length+1);
+            ArrayVtor[j] = ArrayS[i];
+            j++;
+        }
+    }
+        return ArrayVtor;
+}
+
+void Print(string[] ArrayVtor)
+{	
+for (int i = 0; i < ArrayVtor.Length; i++)
+{
+    Console.WriteLine(ArrayVtor[i]);
+}
+}
+
+string[] ArrayS = {"1234", "1567", "-2", "computer science"};
+Print(Izm(ArrayS));
